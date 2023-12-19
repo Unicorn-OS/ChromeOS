1bf414a9d11ce75f7fba32a703a09ca207ca9072
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-12-18 22:00:33 -0600

    Update enable-ssh.sh

--- src/Enable SSH/enable-ssh.sh
+++ src/Enable SSH/enable-ssh.sh
@@ -3,6 +3,9 @@ port=1066
 # Set Port
 sudo sed -i "s/#Port 22/Port $port/g" /etc/ssh/sshd_config
 
+# Remove file
+sudo rm /etc/ssh/sshd_not_to_be_run
+
 # install
 sudo apt -y install openssh-server
 
