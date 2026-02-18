5203196f935188d7f2ad6d718875ba8e5329baed
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-02-18 14:15:21 +0000

    Update SSHD on ChromeOS Crostini Linux.Debian.sh

--- .graph/Enable SSH/SSHD on ChromeOS Crostini Linux.Debian.sh
+++ .graph/Enable SSH/SSHD on ChromeOS Crostini Linux.Debian.sh
@@ -1,2 +1,12 @@
+# Source. https://web.archive.org/web/20210513172249/https://www.hackershark.com/2021/05/13/how-to-ssh-into-your-chromebook-through-linux-beta/
 sudo rm /etc/ssh/sshd_not_to_be_run
 sudo sed -i ´s/# Port/Port 1025/´ /etc/ssh/sshd_config
+
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
+sudo systemctl status ssh
+
+# 5. Set a Root and User Password
+sudo su
+passwd root
